ddd7b1b55b572967f830582b6eeac75483f2aa2d
Eric Abell <ericabell@users.noreply.github.com>
2017-09-02 21:34:01 -0500

    Google OAuth 2 functional

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+let passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+let session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +25,67 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: "http://localhost:3000/auth/redirect"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(`In GoogleStrategy second arg:`);
+    console.log(accessToken, refreshToken, profile);
+
+    // here is where I would extract my app-specific info about this user
+    // and then pass it on in cb as an object.
+    // in this case, I'm just passing the displayName I get back from Google
+    // as username
+
+    // this is my opportunity to take any information from the profile
+    // that I get from Google and pass it along to serializeUser where
+    // that information will be stored in session. If I don't do anything
+    // with the Google profile here, I won't get it back unless the user has
+    // to re-authenticate with Google.
+
+    return cb(null, {profile: profile['_json']})
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+  }
+));
+
+// serializeUser will specify what data we want to be stored in session for
+// this authenticated user. It receives the user object from Google Strategy
+
+passport.serializeUser(function(user, done) {
+  console.log('in serializeUser, receives user from Google Strategy done');
+  console.log(`user:`);
+  console.dir(user);
+  done(null, {profile: user.profile});
+});
+
+// id gets passed whatever Express found in the session for the cookie sent
+// by the user. id is going to be assigned the object we gave as the second
+// argument to done at the end of serializeUser.
+passport.deserializeUser(function(id, done) {
+  console.log('in deserializeUser');
+  console.log(`id:`);
+  console.dir(id);
+  let err = null;
+
+  // now this second argument is what gets placed in req.user by passport.
+  done(err, {displayName: id.profile.displayName, profileImage: id.profile.image.url});
+});
+
+app.use(session({ secret: 'key', // used to sign the session ID cookie
+                  resave: false, // forces session to be saved, even if it didn't change
+                  saveUninitialized: false, // forces an uninitialized session to be saved to the store
+                })
+        );
+
+app.use(passport.initialize());
+app.use(passport.session()); // be sure express-session is BEFORE passport-session
+
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+let passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if( req.user ) {
+    console.log(`req.user:`);
+    console.log(req.user);
+    res.render('index', { title: 'Express', user: req.user.displayName, profileImage: req.user.profileImage})
+  } else {
+    res.render('index', { title: 'Express', user: 'Anonymous', profileImage: '' });
+  }
 });
+
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/auth/redirect',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 module.exports = router;
